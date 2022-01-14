92c328f7e2130133bf04dd1ab3bc585d07277f57
borcherd <borcherd@me.com>
2022-01-14 14:50:46 +0100

    can delete appointment, not yet update

--- src/app/views/pages/apps/calendar/modals/editEvent/editEventForm/edit-event-form.component.ts
+++ src/app/views/pages/apps/calendar/modals/editEvent/editEventForm/edit-event-form.component.ts
@@ -2,6 +2,11 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { EventClickArg } from '@fullcalendar/core';
 import { Subscription } from 'rxjs';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { Appointment } from 'src/app/models/appointment';
+import { Person } from 'src/app/models/person';
+import { AppointmentService } from 'src/app/services/appointment.service';
+import { PersonService } from 'src/app/services/person.service';
 
 @Component({
   selector: 'app-edit-event-form',
@@ -14,14 +19,26 @@ export class EditEventFormComponent implements OnInit {
   @Input() clickInfoInput: EventClickArg;
 
   formEvent: FormGroup;
-  formCustomer: FormGroup;
-  newCustomerShown: boolean = false;
+  currentUser: Person;
 
-  constructor(){
+  constructor(private appointmentService: AppointmentService, private personService: PersonService){
   }
 
   ngOnInit() {
+    console.log(this.clickInfoInput.event.title)
+    this.getCurrentUser();
     this.initForms();
+  }
+
+  /**
+   * gets the current logged in user
+   */
+   getCurrentUser(){
+    this.subscription.add(this.personService.getCurrentUser().subscribe((employee: Person) => {
+      // Setting current user
+      this.currentUser = employee;
+      console.log(employee);
+    }));
   }
 
   /**
@@ -29,9 +46,9 @@ export class EditEventFormComponent implements OnInit {
    */
   initForms(){
     this.formEvent = new FormGroup({
-      startDateTime: new FormControl('', Validators.required),
-      endDateTime: new FormControl('', Validators.required),
-      description:new FormControl('', Validators.required),
+      startDateTime: new FormControl(this.formatDateTimeToDisplay(this.clickInfoInput.event.startStr), Validators.required),
+      endDateTime: new FormControl(this.formatDateTimeToDisplay(this.clickInfoInput.event.endStr), Validators.required),
+      description:new FormControl(this.clickInfoInput.event.title, Validators.required),
     }) ;
   }
 
@@ -39,18 +56,48 @@ export class EditEventFormComponent implements OnInit {
    * registers a new event (and if needed a new customer)
    */
   onSubmit(){
-    console.log(this.formEvent);
-    console.log(this.formCustomer);
-  }
-
-  /**
-   * function to show the div containing the input values of a new customer
-   */
-  toggleShow(){
-    this.newCustomerShown = !this.newCustomerShown;
+    this.subscription.add(this.appointmentService.getAppointmentById(Number(this.clickInfoInput.event.id)).subscribe((appointment: Appointment)=>{
+      const updated = new Appointment(
+        this.formatDateTimeToPush(this.formEvent.controls['startDateTime'].value),
+        this.formatDateTimeToPush(this.formEvent.controls['endDateTime'].value), 
+        true, this.formEvent.controls['description'].value,
+        this.currentUser, appointment.customer
+      )
+      updated.appointmentId = appointment.appointmentId;
+      console.log(updated)
+      this.subscription.add(this.appointmentService.updateAppointment(appointment.appointmentId, updated).subscribe((appointment:Appointment)=>{
+        console.log(updated)
+      }))
+    }))
   }
 
   onDelete(){
-    console.log("delete clicked")
+    this.subscription.add(this.appointmentService.deleteAppointment(Number(this.clickInfoInput.event.id)).subscribe((appointment:Appointment)=>{
+      console.log("deleted appointment")
+    }))
+  }
+
+  /**
+   * formats the datetimestring from the clickevent into a datetimestring to show in the form
+   * @param dateTimeString datetimestring to format
+   * @returns a formatted datetimestring
+   */
+   formatDateTimeToDisplay(dateTimeString:string){
+    const date = dateTimeString.substring(0,10)
+    const time= dateTimeString.substring(11,19)
+
+    return date + " " + time;
+  }
+
+  /**
+   * formats the date to the correct format needed in the database
+   * @param date datetimestring to format
+   * @returns a formatted date
+   */
+  formatDateTimeToPush(date: string) {
+    const temp_date = date.substring(0,10) + "T" + date.substring(11) + "+01:00" ;
+    const temp_date2 = new Date(Date.parse(date));
+    temp_date2.setHours(temp_date2.getHours() + 2);
+    return temp_date2.toISOString();
   }
 }
