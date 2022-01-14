f6802a3f00e5f2dd69f02452c209582a5c12d28b
borcherd <borcherd@me.com>
2022-01-14 18:29:54 +0100

    added date and timepicker to editevent

--- src/app/views/pages/apps/calendar/modals/editEvent/editEventForm/edit-event-form.component.ts
+++ src/app/views/pages/apps/calendar/modals/editEvent/editEventForm/edit-event-form.component.ts
@@ -7,7 +7,7 @@ import { Person } from 'src/app/models/person';
 import { AppointmentService } from 'src/app/services/appointment.service';
 import { PersonService } from 'src/app/services/person.service';
 
-@Component({
+@Component({ 
   selector: 'app-edit-event-form',
   templateUrl: './edit-event-form.component.html'
 })
@@ -45,9 +45,23 @@ export class EditEventFormComponent implements OnInit {
    */
   initForms(){
     this.formEvent = new FormGroup({
-      startDateTime: new FormControl(this.formatDateTimeToDisplay(this.clickInfoInput.event.startStr), Validators.required),
-      endDateTime: new FormControl(this.formatDateTimeToDisplay(this.clickInfoInput.event.endStr), Validators.required),
-      description:new FormControl(this.clickInfoInput.event.title, Validators.required),
+      startDate: new FormControl({
+        year: Number(String(this.clickInfoInput.event.startStr).substring(0,4)),
+        month: Number(String(this.clickInfoInput.event.startStr).substring(5,7)), 
+        day: Number(String(this.clickInfoInput.event.startStr).substring(8,10))}),
+      startTime: new FormControl({
+        hour: Number(String(this.clickInfoInput.event.startStr).substring(11,13)),
+        minute: Number(String(this.clickInfoInput.event.startStr).substring(14,16)),
+        second: 0}),
+      endTime:new FormControl({
+        hour: Number(String(this.clickInfoInput.event.endStr).substring(11,13)),
+        minute: Number(String(this.clickInfoInput.event.endStr).substring(14,16)),
+        second: 0}),
+      endDate: new FormControl({
+        year: Number(String(this.clickInfoInput.event.endStr).substring(0,4)),
+        month: Number(String(this.clickInfoInput.event.endStr).substring(5,7)), 
+        day: Number(String(this.clickInfoInput.event.endStr).substring(8,10))}),
+        description:new FormControl(this.clickInfoInput.event.title, Validators.required),
     }) ;
   }
 
@@ -56,9 +70,11 @@ export class EditEventFormComponent implements OnInit {
    */
   onSubmit(){
     this.subscription.add(this.appointmentService.getAppointmentById(Number(this.clickInfoInput.event.id)).subscribe((appointment: Appointment)=>{
+      const startDateTime = this.createDateTime(this.formEvent.controls['startDate'].value, this.formEvent.controls['startTime'].value)
+      const endDateTime = this.createDateTime(this.formEvent.controls['endDate'].value, this.formEvent.controls['endTime'].value)
       const updated = new Appointment(
-        this.formatDateTimeToPush(this.formEvent.controls['startDateTime'].value),
-        this.formatDateTimeToPush(this.formEvent.controls['endDateTime'].value), 
+        startDateTime,
+        endDateTime, 
         true, this.formEvent.controls['description'].value,
         this.currentUser, appointment.customer
       )
@@ -66,6 +82,8 @@ export class EditEventFormComponent implements OnInit {
       console.log(updated)
       this.subscription.add(this.appointmentService.updateAppointment(appointment.appointmentId, updated).subscribe((appointment:Appointment)=>{
         console.log(updated)
+        this.submitCloseEvent.emit(1)
+
       }))
     }))
   }
@@ -76,30 +94,47 @@ export class EditEventFormComponent implements OnInit {
   onDelete(){
     this.subscription.add(this.appointmentService.deleteAppointment(Number(this.clickInfoInput.event.id)).subscribe((appointment:Appointment)=>{
       console.log("deleted appointment")
+      this.submitCloseEvent.emit(2)
+
     }))
   }
 
+  
   /**
-   * formats the datetimestring from the clickevent into a datetimestring to show in the form
-   * @param dateTimeString datetimestring to format
-   * @returns a formatted datetimestring
+   * turns the input from the date and timepicker into a formatted date
+   * @param date to format
+   * @param time to format
+   * @returns formatted datetime
    */
-   formatDateTimeToDisplay(dateTimeString:string){
-    const date = dateTimeString.substring(0,10)
-    const time= dateTimeString.substring(11,19)
-
-    return date + " " + time;
+   createDateTime(date, time){
+    //"2022-01-13T06:00:00"
+    console.log(date)
+    console.log(time)
+    ;
+    const DateTime = date.year + "-" +
+    this.checkDoubleDigits(date.month) + "-" +
+    this.checkDoubleDigits(date.day) + "T" +
+    this.checkDoubleDigits(time.hour) + ":" + 
+    this.checkDoubleDigits(time.minute) + ":" +
+    "00" + "+01:00";
+    console.log(DateTime)
+    const temp_date = new Date(Date.parse(DateTime));
+    console.log(temp_date)
+    temp_date.setHours(temp_date.getHours() + 2);
+    console.log(temp_date)
+    return temp_date.toISOString();
   }
 
   /**
-   * formats the date to the correct format needed in the database
-   * @param date datetimestring to format
-   * @returns a formatted date
+   * checks if the length of arg is 1
+   * @param arg to check the length of
+   * @returns correct length of arg (has to be 2 for dateobject)
    */
-  formatDateTimeToPush(date: string) {
-    const temp_date = date.substring(0,10) + "T" + date.substring(11) + "+01:00" ;
-    const temp_date2 = new Date(Date.parse(date));
-    temp_date2.setHours(temp_date2.getHours() + 2);
-    return temp_date2.toISOString();
+  checkDoubleDigits(arg){
+    console.log(String(arg).length);
+    if (String(arg).length == 1){
+      arg = "0" + arg
+    }
+    return arg
   }
 }
